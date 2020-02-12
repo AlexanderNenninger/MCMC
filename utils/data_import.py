4556f8f0725d054dbb57ff713255cd55cf7fe10a
Alexander Nenninger <Alexander.Nenninger@nttdata.com>
2020-02-12 13:30:33 +0000

    corrected division

--- utils/data_import.py
+++ utils/data_import.py
@@ -10,7 +10,7 @@ TEST_FILE = 'test.csv'
 def load_observations(data_path=DATA_PATH, fname=TRAIN_FILE) -> (pd.DataFrame, pd.DataFrame):
     train_data = pd.read_csv(data_path / fname)
     train_labels = train_data.pop('label')
-    return (train_data / 255).astype(np.float32), pd.get_dummies(train_labels, prefix="digit", dtype=np.float32)
+    return (train_data / 256).astype(np.float32), pd.get_dummies(train_labels, prefix="digit", dtype=np.float32)
 
 
 if __name__=='__main__':
