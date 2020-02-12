dd9ed5e4a3b7a4c7227c1362bb3ff4525f32b999
Alexander Nenninger <Alexander.Nenninger@nttdata.com>
2020-02-12 13:30:52 +0000

    Enabled GPU

--- utils/forward_op.py
+++ utils/forward_op.py
@@ -1,10 +1,17 @@
 from pathlib import Path
 
 import tensorflow as tf
-tf.config.experimental.set_memory_growth = True
 # Importing the required Keras modules containing model and layers
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+
+
+# GPU Config for RTX Cards
+try:
+    physical_devices = tf.config.list_physical_devices('GPU') 
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except IndexError:
+    print("GPU not used. Resorting to CPU")
 
 # Creating a Sequential Model and adding the layers
 input_shape = (28, 28, 1)
@@ -41,6 +48,7 @@ if __name__=='__main__':
     model.compile(optimizer='adam', 
             loss='sparse_categorical_crossentropy', 
             metrics=['accuracy'])
-    model.fit(x=x_train[:4],y=y_train[:4], epochs=5, batch_size=4)
+
+    model.fit(x=x_train,y=y_train, epochs=5, batch_size=4)
     model.save_weights(str(MODEL_PATH.resolve()))
     model.evaluate(x_test, y_test)
